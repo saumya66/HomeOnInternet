f39084e0998464743ab8cf8e5747bcc2f9d8e5ec
saumyanayak <saumyanayak264@gmail.com>
2022-05-18 16:46:55 +0530

    fixes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.SITE_URL || 'https://saumyanayak.xyz',
     generateRobotsTxt: true, // (optional)
     // ...other options
+    outDir: './out'
   }
