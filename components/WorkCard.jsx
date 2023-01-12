44964d0ad6f06821bcc1b8db07d50bb68a76f8c2
saumyanayak <saumyanayak264@gmail.com>
2023-01-13 01:00:47 +0530

    links added to work cards

--- components/WorkCard.jsx
+++ components/WorkCard.jsx
@@ -4,20 +4,22 @@ import Image from "next/image"
 
 const WorkCard = ({ work, index }) => {
     return <div className={index % 2 ? styles.rightWorkCard : styles.leftWorkCard}>
-        <div className={styles.workContainer}>
-            <div className={styles.topContainer}>
-                <div style={{ borderRadius: '15px', overflow: 'hidden', width: "50px", height: "50px" }} >
-                    <Image src={work.thumbnail} alt="" width="50" height="50" />
+        <a href={work.link} target="_blank" rel="noreferrer">
+            <div className={styles.workContainer}>
+                <div className={styles.topContainer}>
+                    <div style={{ borderRadius: '15px', overflow: 'hidden', width: "50px", height: "50px" }} >
+                        <Image src={work.thumbnail} alt="" width="50" height="50" />
+                    </div>
+                    <div style={{ height: '100%', width: '78%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginLeft: '10px', paddingTop: '4px', paddingBottom: '4px' }}>
+                        <p style={{ fontSize: '1rem', fontWeight: 'bold' }}>{work.name}</p>
+                        <p style={{ fontSize: '0.8rem' }}>{work.role}</p>
+                    </div>
                 </div>
-                <div style={{ height: '100%', width: '78%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginLeft: '10px', paddingTop: '4px', paddingBottom: '4px' }}>
-                    <p style={{ fontSize: '1rem', fontWeight: 'bold' }}>{work.name}</p>
-                    <p style={{ fontSize: '0.8rem' }}>{work.role}</p>
+                <div className={styles.bottomContainer}>
+                    <p style={{ fontSize: '0.7rem' }}>{work.contribution}</p>
                 </div>
             </div>
-            <div className={styles.bottomContainer}>
-                <p style={{ fontSize: '0.7rem' }}>{work.contribution}</p>
-            </div>
-        </div>
+        </a>
     </div>
 }
 
