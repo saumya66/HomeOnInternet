8b4a883c66ed4de4e4bca3df070dd8b66c12e55d
saumya66 <saumyanayak264@gmail.com>
2024-05-05 14:48:41 +0530

    lets shipp

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import {useRouter} from "next/router"
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
   const canonicalUrl = (`https://saumyanayak.xyz` + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
 
   return (
-    <div >
+    <div className=''>
       <Head>
           <title>Home - Saumya Ranjan Nayak</title>
           <meta name="description" content="Hello! This is Saumya Ranjan Nayak's home on the internet." />
@@ -18,27 +19,16 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
           <link rel="canonical" href={canonicalUrl} />
       </Head>
-      <div className='py-8'>
-        <p className='text-lg pb-8'>{"hi i'm saumya, working on making this site simple. "}</p>
-          {/*  <p className='text-lg underline pb-1'>focusing currently on</p>
-         <ul className='list-disc pl-4'>
-          <li>contributing to leap.club to make a beautiful community of women.</li>
-          <li>getting back to learning and building mode</li>
-          <li>making waking up early in the morning a habit</li>
-          <li>hitting gym 6 days a week</li>
-          <li>writing consistently</li>
-         </ul>
-        <p className='text-lg underline pt-4'>currently reading</p>
-        <ul className='list-disc pl-4'>
-          <li>what are you doing with your life? by jiddu krishnamurti</li>
-          <li>elon musk by walter isaacson</li>
-        </ul> */}
-        {/* <p className='text-lg underline pt-4'>currently writing</p> */}
-        
+      <div className='pt-12 sm:py-16 sm:w-9/12'>
+        <p className='text-base sm:text-lg pb-6'>{"hi there. welcome to my little corner on the internet."}</p>
+        <p className='text-base sm:text-lg pb-6'>{"quick intro. spawned on earth in the year 2000. discovered programming in 2020. fell in love with learning to make or making to learn. started working in 2021. been working at startups & on multiple side-projects building cool things. i build across the stack but have heavily worked on the frontend - building & shipping mobile and web apps."}</p>
+        <p className='text-base sm:text-lg pb-6'>{"apart from this i enjoy reading good books, drawing - particularly pencil sketches, listening to good music, often singing too and definitely a good workout."}</p>
+        <p className='text-base sm:text-lg pb-6 font-medium'>goal is to seek truth and work on useful things that reduce suffering.</p>
+        <p className='text-base pb-2'>⚒️ currently building: <span className='underline'><Link href={"https://x.com/saumyanayak_/status/1786658748976488931"}>an image optimizer</Link></span> </p>
+        <p className='text-base pb-4'>✍️ currently writing: <span className='italic'><Link href={""}>nothing</Link></span> </p>
+        <p className='text-base pb-2'>⚒️ last sideproject: <span className='underline'><Link href={"/projects"}>sendecentralized</Link></span> </p>
+        <p className='text-base pb-4'>📝 last blog: <span className='underline'><Link href={"/blogs/understanding-ethereum-white-paper-part-1"}>understanding ethereum white paper </Link></span> </p>
       </div>
-      <div>
-      </div>
-      
     </div>
   )
 }
